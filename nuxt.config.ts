2e7a51bde6fb85591813a52a3e65f448fbf71317
251152 <251152@edu.p.lodz.pl>
2026-03-30 14:17:42 +0200

    Refactor code structure for improved readability and maintainability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,8 @@ export default defineNuxtConfig({
 		//"@nuxtjs/tailwindcss",
 	],
 	supabase: {
+		url: process.env.SUPABASE_URL,
+    	key: process.env.SUPABASE_ANON_KEY,
 		redirectOptions: {
 			login: "/login",
 			exclude: ["/register", "/login", "/"],
