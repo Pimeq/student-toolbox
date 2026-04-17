33b0acf9bfc587f4f1730edea5c716aee56fa3a6
pimeq <54513008+Pimeq@users.noreply.github.com>
2026-04-17 17:07:24 +0200

    moved the calendar to the corect route, s well as implemented a navbar for navigation

---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+	const supabase = useSupabaseClient()
+	const user = useSupabaseUser()
+	//TODO:
+	//fetch user groups, if not any -> show signup dialog
+	//some side panel with notes events and whatnot
+
+	definePageMeta({
+		layout: "dashboard",
+	})
+
+	const { data: userGroups } = await useAsyncData(
+		"userGroups",
+		async () => {
+			if (!user.value) return []
+
+			const { data, error } = await supabase
+				.from("user_memberships")
+				.select("*")
+				.eq("user_id", user.value.sub)
+			if (error) throw error
+			return data ?? []
+		},
+		{
+			default: () => [],
+		},
+	)
+
+	const { data: res } = await useAsyncData("bucket", async () => {
+		const { data } = await supabase.storage.from("files").list("generic")
+		return data
+	})
+</script>
+
+<template>
+	<NewUserModal :show="userGroups.length == 0" />
+	<UDashboardPanel>
+		<template #header>
+			<UDashboardNavbar title="Dashboard">
+				<template #leading>
+					<UDashboardSidebarCollapse />
+				</template>
+			</UDashboardNavbar>
+			{{ res }}
+		</template>
+	</UDashboardPanel>
+</template>
